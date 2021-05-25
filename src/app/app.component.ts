312daa364333c48e9559aa0464eaa792eda0852e
Suresh kumar <sureshece9999@gmail.com>
2021-05-25 07:52:55 +0530

    Css fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ export class AppComponent {
     { title: 'Home', url: '/home', icon: 'assets/images/home.png' },
     { title: 'Html', url: '/html', icon: 'assets/images/html.png' },
     { title: 'Angular', url: '/angular', icon: 'assets/images/angular.png' },
+    { title: 'Javascript', url: '/folder/Favorites', icon: 'assets/images/js.png' },
     { title: 'JQuery', url: '/folder/Archived', icon: 'assets/images/jquery.png' },
     { title: 'CSS', url: '/folder/Trash', icon: 'assets/images/css.png' },
-    { title: 'Javascript', url: '/folder/Favorites', icon: 'assets/images/js.png' },
     { title: 'Bootstrap', url: '/folder/Spam', icon: 'assets/images/bootstrap.png' },
   ];
 
